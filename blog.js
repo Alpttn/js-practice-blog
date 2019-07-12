fa2f7be520467e14b36f531e63c7d92b92811de8
Allie <allison.tpttn@gmail.com>
2019-07-11 20:58:11 -0500

    created and linked files, changed header content with js

---
+++ blog.js
@@ -0,0 +1,6 @@
+// document.querySelector(".article_header").textContent = "Welcome to the Allison blog";
+
+const article__header = document.querySelector(".article__header");
+article__header.textContent = "Welcome to the Allison blog";
+
+
